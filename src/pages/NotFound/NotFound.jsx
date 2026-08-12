95e473356873fc5cc627e004df0a6de2073a3133
Hirman <i.hirman2006@gmail.com>
2026-08-12 12:04:34 +0430

    feat: add notfound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,20 @@
-import './NotFound.css'
+import { Link } from "react-router";
+import "./NotFound.css";
 
-import React from 'react'
+import React from "react";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <>
+      <div className="notfound">
+        <h1 className="notfound__code">404</h1>
+        <h2 className="notfound__text">صفحه مورد نظر یافت نشد!</h2>
+        <button className="redirect__dashboard">
+          <Link to={"/"}>داشبورد</Link>
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default NotFound
+export default NotFound;
--- src/router.jsx
+++ src/router.jsx
@@ -1,27 +1,27 @@
-import {createBrowserRouter} from "react-router"
-import MainLayout from './layouts/MainLayout'
-import Transaction from './pages/Transaction/Transaction'
-import Category from './pages/Category/Category'
-import Report from './pages/Report/Report'
-import FinancialGoal from './pages/FinancialGoal/FinancialGoal'
-import Dashboard from "./pages/Dashboard/Dashboard"
-import NotFound from "./pages/NotFound/NotFound"
-import ErrorPage from './pages/ErrorPage/ErrorPage'
+import { createBrowserRouter } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import Transaction from "./pages/Transaction/Transaction";
+import Category from "./pages/Category/Category";
+import Report from "./pages/Report/Report";
+import FinancialGoal from "./pages/FinancialGoal/FinancialGoal";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import NotFound from "./pages/NotFound/NotFound";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<MainLayout />,
-        errorElement:<ErrorPage />,
-        children:[
-            {index:true,element:<Dashboard />},
-            {path:'transaction',element:<Transaction />},
-            {path:'category',element:<Category />},
-            {path:'report',element:<Report />},
-            {path:'financialgoal',element:<FinancialGoal />},
-            {path:'*',element:<NotFound />}
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: "transaction", element: <Transaction /> },
+      { path: "category", element: <Category /> },
+      { path: "report", element: <Report /> },
+      { path: "financialgoal", element: <FinancialGoal /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
 
 export default router;
