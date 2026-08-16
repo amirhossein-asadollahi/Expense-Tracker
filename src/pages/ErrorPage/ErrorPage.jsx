b2408eb61b217137134e8aa8c4b6aee98fb7eb59
Hirman <i.hirman2006@gmail.com>
2026-08-16 12:46:23 +0430

    feat: add error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,20 @@
-import './ErrorPage.css'
+import { Link } from "react-router";
+import "./ErrorPage.css";
 
-import React from 'react'
+import React from "react";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <>
+      <div className="notfound">
+        <h1 className="notfound__code">ERROR</h1>
+        <h2 className="notfound__text">مشکلی پیش آمده!!!</h2>
+        <button className="redirect__dashboard">
+          <Link to={"/"}>داشبورد</Link>
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
